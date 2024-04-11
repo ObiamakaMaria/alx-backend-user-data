8bb65301edd0b448bf47bc0b40c2fcdbab03452f
ObiamakaMaria <obiamaka0101@gmail.com>
2024-04-11 22:13:25 +0000

    encrypt_password.py

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Password encryption utilities"""
+
+import bcrypt
+
+def hash_password(password: str) -> bytes:
+    """Hashes a password"""
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(password.encode(), salt)
+    return hashed
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    '''Checks if a hashed password matches a given password'''
+    if bcrypt.checkpw(password.encode(), hashed_password):
+        return True
+    return False
